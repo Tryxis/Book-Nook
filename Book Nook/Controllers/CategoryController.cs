761c8d1e9b62b4edb8bcda5ff21ed6548f42b3ea
Tryxis <47526388+Tryxis@users.noreply.github.com>
2024-11-21 12:23:23 +0000

    Updated css

--- Book Nook/Controllers/CategoryController.cs
+++ Book Nook/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Book_Nook.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
@@ -9,9 +10,16 @@ namespace Book_Nook.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var categoryList = _context.Categories.ToList();
+            return View(categoryList);
         }
     }
 }
