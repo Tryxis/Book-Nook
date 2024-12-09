568efc026d409aa2fd24acdfcde292e04ccfa253
Tryxis <47526388+Tryxis@users.noreply.github.com>
2024-12-09 20:16:46 +0000

    CRUD operations

--- Book Nook/Controllers/CategoryController.cs
+++ Book Nook/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Book_Nook.Data;
+using Book_Nook.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
@@ -25,6 +26,78 @@ namespace Book_Nook.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+            
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            } 
+
+            var categorytoEdit = _context.Categories.FirstOrDefault(x => x.Id == id);
+            if(categorytoEdit == null)
+            {
+                return NotFound();
+            }
+
+            return View(categorytoEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+            
+        }
+        public IActionResult Delete(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            } 
+
+            var categorytoDelete = _context.Categories.FirstOrDefault(x => x.Id == id);
+            if(categorytoDelete == null)
+            {
+                return NotFound();
+            }
+
+            return View(categorytoDelete);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteFromDb(int? id)
+        {
+            Category? categorytoDelete = _context.Categories.Find(id);
+            if(categorytoDelete == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(categorytoDelete);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+            
         }
     }
 }
--- Book Nook/Models/Category.cs
+++ Book Nook/Models/Category.cs
@@ -13,10 +13,12 @@ namespace Book_Nook.Models
         public int Id { get; set; }
 
         [Required]
+        [MaxLength(30)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
 
         [DisplayName("Display Order")]
+        [Range(1,100)]
         public int DisplayOrder { get; set; }
     }
 }
